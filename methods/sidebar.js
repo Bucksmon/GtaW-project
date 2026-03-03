5f7d95dd75e7fa24649a0b65a7fb89b90eaeda64
Bucksmon <74790878+Bucksmon@users.noreply.github.com>
2026-03-03 17:09:16 +0500

    change 67

--- methods/sidebar.js
+++ methods/sidebar.js
@@ -49,7 +49,7 @@ const currentPage = window.location.pathname.split("/").pop();
       ${menuItems
         .map(
           (item) => `
-          <li class="${item.href.split("/").pop() === "dashboard.html" ? "active" : ""}">
+          <li class="${item.href.split("/").pop() === currentPage ? "active" : ""}">
             <a href="${item.href}">
               <span>${item.icon}</span>
               ${item.label}
