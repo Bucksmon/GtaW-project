79e0e1a735f6167d10d21a0eb202d3791468b9ca
Bucksmon <74790878+Bucksmon@users.noreply.github.com>
2026-03-05 01:01:39 +0500

    change 67

--- methods/sidebar.js
+++ methods/sidebar.js
@@ -7,7 +7,6 @@ function getMenuByRole(role) {
 
   const userMenu = [
     { label: "My Orders", href: "../Buyer/buyerOrder.html", icon: "📦" },
-    { label: "My Services", href: "../Seller/removeService.html", icon: "🛠" },
     { label: "Add Service", href: "../Seller/addService.html", icon: "➕" },
     { label: "Credits", href: "../Buyer/addCredits.html", icon: "💳" },
   ];
